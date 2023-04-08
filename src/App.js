96a911b1bd6015c09d56cdcf377cbd60768725a8
Taif Ahmad <a7x.taif@gmail.com>
2023-04-08 23:20:23 +0530

    added entire project code

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from "react";
+import Header from "./Components/Layout/Header";
+import Meals from "./Components/Meals/Meals";
+
+function App() {
+  return (
+    <Fragment>
+      <Header />
+      <main>
+        <Meals />
+      </main>
+    </Fragment>
+  );
+}
+
+export default App;
